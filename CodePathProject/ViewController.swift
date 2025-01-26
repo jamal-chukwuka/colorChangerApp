e6ce0ad9934678f5e6839c1cd43cc108ca140710
Jamal Nezianya <jnezia1@hotmail.com>
2025-01-26 08:31:53 -0600

    added new features

--- CodePathProject/ViewController.swift
+++ CodePathProject/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var  clickCount = 0
+    
+    @IBOutlet var labels: [UILabel]!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,18 +20,36 @@ class ViewController: UIViewController {
 
 
     @IBAction func changeBackgroundColor(_ sender: UIButton) {
-        let randomColor = changeColor()
+        
+        clickCount += 1
+        let isFill = clickCount % 3 == 0
+        let randomColor = changeColor(fill: isFill)
         view.backgroundColor = randomColor
+        
+        sender.setTitleColor(randomColor, for: .normal)
+        
+        guard let labels = labels else {
+                print("Labels array is nil!")
+                return
+        }
+        
+        for label in labels {
+            label.textColor = randomColor
+            label.backgroundColor = isFill ? randomColor.withAlphaComponent(0.3) : UIColor.clear
+        }
         
     }
     
-    func changeColor() -> UIColor{
+    func changeColor(fill: Bool) -> UIColor{
 
             let red = CGFloat.random(in: 0...1)
             let green = CGFloat.random(in: 0...1)
             let blue = CGFloat.random(in: 0...1)
+            let alpha: CGFloat = fill ? 1.0 : 0.5
 
-            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            return UIColor(red: red, green: green, blue: blue, alpha: alpha)
         }
+    
+    
 }
 
